e41ef6e64eecb71f2bad14ddc97fe6eb27aff2f4
kgeneral <dykim1983@gmail.com>
2020-02-22 08:30:49 +0900

    use hashmap for pick second value

--- dykim/amazon.two_sum.py
+++ dykim/amazon.two_sum.py
@@ -1,13 +1,14 @@
 # https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/508/
 # elapsed time : around 5 min
-# TODO : do more TC optimization
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                if i==j: continue
-                if nums[i]+nums[j]==target: return [i,j] 
-            
-        return []       
+        numsDict = {}
+        for idx, val in enumerate(nums):
+            numsDict[val] = idx 
+        for idx, val in enumerate(nums):
+            diff = target - val
+            if diff in numsDict:
+                if idx == numsDict[diff]: continue
+                return [idx, numsDict[diff]]
